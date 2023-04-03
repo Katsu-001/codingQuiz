e5c7cd29f13905d7b30cc95af0ca1b85e7d39c7d
Katsuyuki <katsuyuki.russell@gmail.com>
2023-04-03 17:03:32 -0500

    added high scrore page, and built body for question on html

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,12 @@
-var timerElement = document.querySelector(".timer-count");
-var startButton = document.querySelector(".start-button");
-
 
 var isWin = false;
-var timer;
-var timerCount;
+var saeconds = 60;
+var shuffledQuestions, currentQuestions;
 
 // The startGame function is called when the start button is clicked
 function startGame() {
     isWin = false;
-    timerCount = 10;
     // Prevents start button from being clicked when round is in progress
     startButton.disabled = true;
-    renderBlanks()
-    startTimer()
+
   }
