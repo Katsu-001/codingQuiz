ce73e84c1101f3674f736b5007f13f294483b208
Katsuyuki <katsuyuki.russell@gmail.com>
2023-04-03 17:38:00 -0500

    added question variables to JS

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,53 @@
+// Question, Choices, and the Answers
+var questions = [
+  {
+    title: 'What do arrays in javascript store?',
+    choices: [
+        { text: 'booleans', correct: false},
+        { text: 'strings', correct: false},
+        { text: 'numbers', correct: false},
+        { text: 'all of the above', correct: true}
+    ]
+  },
+  {
+    title: 'Which of the following is used to define a variable in Javascript?',
+    choices: [
+        { text: 'let', correct: false},
+        { text: 'var', correct: false},
+        { text: 'neither', correct: false},
+        { text: 'both', correct: true}
+    ]
+},
+{
+  title: 'Some regularly used data types DO NOT include:',
+  choices: [
+      { text: 'strings', correct: false},
+      { text: 'booleans', correct: false},
+      { text: 'alerts', correct: true},
+      { text: 'numbers', correct: false}
+  ]
+},
+{
+  title: 'Which is the correct HTML element used to store our JavaScript link',
+  choices: [
+      { text: 'script', correct: true},
+      { text: 'javascript', correct: false},
+      { text: 'scripting', correct: false},
+      { text: 'js', correct: false}
+  ]
+},
+{
+  title: 'What is used encolosing a string to assign it to a variable?',
+  choices: [
+      { text: 'commas', correct: false},
+      { text: 'quotes', correct: true},
+      { text: 'curly brackets', correct: false},
+      { text: 'parentheses', correct: false}
+  ]
+}
+];
 
-var isWin = false;
-var saeconds = 60;
-var shuffledQuestions, currentQuestions;
 
 // The startGame function is called when the start button is clicked
 function startGame() {
-    isWin = false;
-    // Prevents start button from being clicked when round is in progress
-    startButton.disabled = true;
-
-  }
+}
